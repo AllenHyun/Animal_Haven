f1244c287e944d98fddb9495742da40ae25e703b
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-03-22 18:14:41 +0000

    The login page has been added to the web

--- animal-haven/src/app/app.routes.ts
+++ animal-haven/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import {Search} from './pages/search/search';
 import {Register} from './pages/register/register';
 import {Home} from './pages/home/home';
+import {Login} from './pages/login/login';
 export const routes: Routes = [
   {path: '',component:Home},
   {path: 'sign-up', component: Register},
-  {path: 'search', component: Search}
+  {path: 'search', component: Search},
+  {path: 'login', component: Login},
 ];
