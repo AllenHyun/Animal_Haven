c366e6bbb245cc63bf4240539bc5efcad7b0d602
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-04-11 19:49:01 +0100

    Profile and booking form done

--- animal-haven/src/app/app.routes.ts
+++ animal-haven/src/app/app.routes.ts
@@ -3,9 +3,13 @@ import {Search} from './pages/search/search';
 import {Register} from './pages/register/register';
 import {Home} from './pages/home/home';
 import {Login} from './pages/login/login';
+import {Booking} from './pages/booking/booking';
+import {Profile} from './pages/profile/profile';
 export const routes: Routes = [
   {path: '',component:Home},
   {path: 'sign-up', component: Register},
   {path: 'search', component: Search},
   {path: 'login', component: Login},
+  {path: 'booking', component: Booking},
+  {path: 'profile', component: Profile},
 ];
