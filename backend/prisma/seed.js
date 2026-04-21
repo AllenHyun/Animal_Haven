af973ebcec058a61e5f65fcd7dece4fce1657eed
Leslie Romero <ll.rommar@gmail.com>
2026-04-21 17:03:47 +0100

    Change seed logic from create to upsert

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -6,11 +6,16 @@ const prisma = require("../src/config/prisma");
 async function main() {
   console.log("Starting seed...");
 
-  await prisma.pet.createMany({
-    data: animalData,
-    skipDuplicates: true,
-  });
-  console.log(`Seeded ${animalData.length} users.`);
+  let petsUpserted = 0;
+  for (const pet of animalData) {
+    await prisma.pet.upsert({
+      where: { id: pet.id },
+      update: pet,
+      create: pet,
+    });
+    petsUpserted++;
+  }
+  console.log(`Upserted ${petsUpserted} pets.`);
 }
 
 main()
