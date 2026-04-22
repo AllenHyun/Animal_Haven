ea7bcb4af334e52c77e6156f97a63539e0115140
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-04-22 11:42:56 +0100

    If there is an error, the system will tell the user to fix it during login or registration

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
@@ -13,6 +14,7 @@ export class Login {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
 
   loginForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -23,7 +25,10 @@ export class Login {
   errorMessage = '';
 
   login() {
-    if (this.loginForm.invalid) return;
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
 
     this.isLoading = true;
     this.errorMessage = '';
@@ -32,16 +37,18 @@ export class Login {
       next: (response) => {
         console.log('Logged in!');
         this.isLoading = false;
+        this.cdr.detectChanges();
         this.router.navigate(['/']);
       },
 
       error: (err) => {
         this.isLoading = false;
-        if (err.status === 401) {
+        if (err.status === 401 || err.status === 404) {
           this.errorMessage = 'Incorrect email or password.';
         } else {
           this.errorMessage = 'Server error. Please try again later.';
         }
+        this.cdr.detectChanges();
       },
     });
   }
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {ChangeDetectorRef, Component, inject} from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
@@ -14,35 +14,54 @@ export class Register {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
 
   signUpForm: FormGroup = this.fb.group({
-    firstName: ['', [Validators.required]],
-    lastName: ['', [Validators.required]],
+    firstName: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', [Validators.required, Validators.minLength(2)]],
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]],
-    postcode: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
+    postcode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
+  }, {
+    validators: this.passwordMatchValidator
   });
 
   isLoading = false;
   errorMessage = '';
 
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    return password && confirmPassword && password.value !== confirmPassword.value ? { passwordMismatch: true } : null;
+  }
+
   signup() {
-    if (this.signUpForm.invalid) return;
+    if (this.signUpForm.invalid) {
+      this.signUpForm.markAllAsTouched();
+      return;
+    }
 
     this.isLoading = true;
     this.errorMessage = '';
+    this.cdr.detectChanges();
 
     this.authService.register(this.signUpForm.value).subscribe({
       next: (response) => {
-        console.log('Backend says:', response);
         this.isLoading = false;
         this.router.navigate(['/login']);
       },
 
       error: (err) => {
-        console.error('Registration failed:', err);
         this.isLoading = false;
-        this.errorMessage = err.error?.error || 'An unexpected error occurred.';
+        const backendError = err.error?.error || '';
+
+        if (backendError.includes('User_email_key')) {
+          this.errorMessage = 'This email is already in use. Please use another one.';
+        } else {
+          this.errorMessage = 'An unexpected error occurred. Please try again.';
+        }
+        this.cdr.detectChanges();
       },
     });
   }
