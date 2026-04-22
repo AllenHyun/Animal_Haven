4212d47a79e79325adbf7e91262ce8898b76395e
María Cabrera <126077150+AllenHyun@users.noreply.github.com>
2026-04-22 14:03:39 +0100

    Booking validation

--- frontend/src/app/pages/booking/booking.ts
+++ frontend/src/app/pages/booking/booking.ts
@@ -1,24 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
 import { Observable } from 'rxjs';
 import { BookingService } from '../../services/booking.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
-  imports: [CommonModule, FormsModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './booking.html',
   styleUrl: './booking.css',
 })
 export class Booking implements OnInit {
+  private fb = inject(FormBuilder);
+  private bookingService = inject(BookingService);
+  private cdr = inject(ChangeDetectorRef);
+
   shelters$!: Observable<any[]>;
   timeFrames$!: Observable<any[]>;
   isShelterSelected = false;
 
-  constructor(private bookingService: BookingService) {}
+  bookingForm: FormGroup = this.fb.group(
+    {
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      confirmEmail: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+    },
+    {
+      validators: this.emailMatchValidator,
+    },
+  );
+
+  constructor() {}
 
   ngOnInit(): void {
     this.shelters$ = this.bookingService.getShelters();
+  }
+
+  emailMatchValidator(control: AbstractControl) {
+    const email = control.get('email');
+    const confirm = control.get('confirmEmail');
+    return email && confirm && email.value !== confirm ? { emailMismatch: true } : null;
   }
 
   onShelterChange(event: Event) {
@@ -31,5 +55,14 @@ export class Booking implements OnInit {
     } else {
       this.isShelterSelected = false;
     }
+  }
+
+  submitBooking() {
+    if (this.bookingForm.invalid) {
+      this.bookingForm.markAllAsTouched();
+      this.cdr.detectChanges();
+      return;
+    }
+    console.log('Formulario válido', this.bookingForm.value);
   }
 }
